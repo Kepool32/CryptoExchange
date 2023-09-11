b849c8b5146012d2021ed9e30023d4d68de5a308
Kepool32 <kukso.artem@mail.ru>
2023-09-11 17:19:59 +0300

    Added CoinInfo component

---
+++ src/components/CoinInfo.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useGetCoinsQuery } from '../service/coinApi';
+import Loader from './Loader';
+import Icon from 'react-crypto-icons';
+import PriceChart from './PriceChart';
+import "../style/CoinInfo.scss"
+import {formatValue} from "../formattingAndSorting/PriceFormatting";
+
+const CoinInfo = () => {
+    const { coinId } = useParams();
+    const history = useNavigate();
+    const { data: coinData, isLoading, isError } = useGetCoinsQuery({
+        limit: 1,
+        search: coinId,
+    });
+
+    if (!coinId) {
+        return <div>Coin ID is missing</div>;
+    }
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    if (isError) {
+        return <div>Error loading coin data</div>;
+    }
+
+    if (!coinData || !coinData.data.length) {
+        return <div>Coin not found</div>;
+    }
+
+    const coin = coinData.data[0];
+
+    return (
+        <div className="coin-info-container">
+            <div className="coin-symbol">
+                    <h1>{coin.name}</h1>
+                    <Icon name={coin.symbol.toLowerCase()} size={150} />
+             </div>
+            <div className="coin-info">
+                <p>Symbol: {coin.symbol}</p>
+                <p>Rank: {coin.rank}</p>
+                <p>Supply: {formatValue(parseFloat(coin.supply))}</p>
+                <p>Price (USD): {formatValue(parseFloat(coin.priceUsd))}</p>
+                <p>Market Cap (USD): {formatValue(parseFloat(coin.marketCapUsd))}</p>
+                <p>Max Supply: {formatValue(parseFloat(coin.maxSupply))}</p>
+            </div>
+                <PriceChart coinId={coinId} />
+                <div className="buttons-container">
+                    <button className="add-to-portfolio-button">Add to Portfolio</button>
+                    <button className="go-back-button" onClick={() => history('/')}>Go Back</button>
+                </div>
+        </div>
+    );
+};
+
+export default CoinInfo;
