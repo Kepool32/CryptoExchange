0ac304150f95fee3db4eeb172241a19dafddf81c
Kepool32 <kukso.artem@mail.ru>
2023-09-11 17:20:09 +0300

    Added PriceChart component

---
+++ src/components/PriceChart.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { useGetPriceDataQuery } from '../service/coinApi';
+import '../style/PriceChart.scss';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { format } from 'date-fns';
+import Loader from "./Loader";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+type PriceChartProps = {
+    coinId: string;
+};
+
+type PriceDataItem = {
+    date: string;
+    priceUsd: string;
+};
+
+const PriceChart: React.FC<PriceChartProps> = ({ coinId }) => {
+    const [selectedInterval, setSelectedInterval] = useState('m1');
+
+    const { data: priceData, isLoading, isError } = useGetPriceDataQuery({
+        coinId,
+        interval: selectedInterval,
+    }, {
+        pollingInterval: 100000,
+    });
+
+    if (isLoading) {
+        return <Loader/>;
+    }
+
+    if (isError) {
+        return <div>Error loading price data</div>;
+    }
+
+    const transformData = (data: PriceDataItem[] | undefined) => {
+        if (!data) {
+            return {
+                labels: [],
+                datasets: [],
+            };
+        }
+
+        return {
+            labels: data.map((item) => format(new Date(item.date), 'yyyy-MM-dd HH:mm')),
+            datasets: [
+                {
+                    label: 'Price (USD)',
+                    data: data.map((item) => parseFloat(item.priceUsd)),
+                    borderColor: 'rgb(39, 227, 25)',
+                    backgroundColor: 'rgba(37, 129, 33, 0.5)',
+                    borderWidth: 1.75,
+                    pointRadius: 1,
+                },
+            ],
+        };
+    };
+
+    return (
+        <div className="price-chart-container">
+            <h2>Price Chart for {coinId}</h2>
+            <select
+                value={selectedInterval}
+                onChange={(e) => setSelectedInterval(e.target.value)}
+            >
+                <option value="m1">Day</option>
+                <option value="m15">7 Day</option>
+                <option value="h1">Month</option>
+            </select>
+            <Line data={transformData(priceData?.data)} />
+        </div>
+    );
+};
+
+export default PriceChart;
