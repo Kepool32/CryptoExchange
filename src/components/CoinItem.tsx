e1938632e167a8c0a98d580904b4c9eeffe2c8d7
Kepool32 <kukso.artem@mail.ru>
2023-09-11 17:18:43 +0300

    Added CoinItem component

--- src/components/CoinItem.tsx
+++ src/components/CoinItem.tsx
@@ -3,17 +3,24 @@ import Icon from 'react-crypto-icons';
 
 import { Coin } from '../types/Coin';
 import '../style/CoinTable.scss';
-import {formatValue} from "../formattingAndSorting/PriceFormatting";
+import { formatValue } from '../formattingAndSorting/PriceFormatting';
 
 interface CoinItemProps {
     coin: Coin;
 }
 
 const CoinItem: React.FC<CoinItemProps> = ({ coin }) => {
+    const handleRowClick = () => {
+
+        window.location.href = `/coins/${coin.id}`;
+    };
+
     return (
-        <tr key={coin.id}>
+        <tr className="coin-row" onClick={handleRowClick}>
             <td>{coin.symbol}</td>
-            <td><Icon name={coin.symbol.toLowerCase()} size={25} /></td>
+            <td>
+                <Icon name={coin.symbol.toLowerCase()} size={25} />
+            </td>
             <td>{formatValue(parseFloat(coin.priceUsd))}</td>
             <td>{formatValue(parseFloat(coin.marketCapUsd))}</td>
             <td>{(parseFloat(coin.changePercent24Hr) * 100).toFixed(2)}%</td>
