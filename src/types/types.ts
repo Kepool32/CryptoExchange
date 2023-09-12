03450336f1ca2c786baf134affcb6890e20b6e0a
Kepool32 <kukso.artem@mail.ru>
2023-09-13 00:32:50 +0300

    Added and separated types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,6 @@
-export interface Coin {
+import React from "react";
+
+export interface Types {
     id: string;
     rank: string;
     symbol: string;
@@ -10,4 +12,100 @@ export interface Coin {
     priceUsd: string;
     changePercent24Hr: string;
     vwap24Hr: string;
+}
+
+export interface CoinItemProps {
+    coin: Types;
+}
+
+
+export interface CoinTableProps {
+    sortedData: Types[];
+    onSortByPrice: () => void;
+    onSortByMarketCap: () => void;
+    onSortByChange: () => void;
+}
+
+
+
+
+export type GetCoinsParams = {
+    limit: number;
+    search?: string;
+};
+
+export type GetPriceDataParams = {
+    coinId: string;
+    interval: string;
+};
+export type PriceDataItem = {
+    time:number;
+    date: string;
+    priceUsd: string;
+
+};
+
+export interface PurchaseData {
+    coinSymbol: string;
+    quantity: number;
+    price: number;
+}
+
+
+export type BuyCryptoModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+    coinSymbol: string;
+    price: string;
+};
+
+
+
+export type PortfolioDataItem = {
+    coinSymbol: string;
+    quantity: number;
+    price: number;
+};
+
+export type PortfolioModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+};
+
+
+export type CryptoData = {
+    name: string;
+    priceUsd: number;
+};
+
+export type PortfolioItem = {
+    quantity: number;
+    price: number;
+};
+
+
+
+export type PriceChartProps = {
+    coinId: string;
+};
+
+export type AddToPortfolioButtonProps = {
+    symbol: string;
+    price: string;
+};
+
+
+export interface PaginationButtonProps {
+    handleNextPage: () => void;
+}
+
+
+export interface SortButtonProps {
+    label: string;
+    onClick: () => void;
+}
+
+export interface SearchBarProps {
+    searchQuery: string;
+    setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 }
