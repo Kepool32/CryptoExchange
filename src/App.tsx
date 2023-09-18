cea5857c8c00818236f05f0a01491dda6cbff2ad
Kepool32 <109909825+Kepool32@users.noreply.github.com>
2023-09-19 01:40:23 +0300

    Update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import router from "./routing/routes";
+import router from "./routes/routes";
 import {RouterProvider} from "react-router-dom";
 import Header from "./components/Header/Header";
 
