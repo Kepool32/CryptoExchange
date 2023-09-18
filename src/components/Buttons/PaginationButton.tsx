49bff7189480d82e5bff836c0219838ef08799a7
Kepool32 <kukso.artem@mail.ru>
2023-09-19 01:28:09 +0300

    Added 'Back' button and updated pagination logic

--- src/components/Buttons/PaginationButton.tsx
+++ src/components/Buttons/PaginationButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./style/PaginationButton.scss"
+import "./styles/PaginationButton.scss"
 
 export interface PaginationButtonProps {
     handlePrevPage: () => void;
